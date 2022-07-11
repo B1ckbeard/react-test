eb588450e66c76392f639c9acd4d3abfdaa19056
BlackBeard <evs.diman@yandex.ru>
2022-07-11 13:46:41 +0300

    React-test

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './app-header.css';
+
+const AppHeader = () =>{
+    return(
+        <div className="app-header d-flex">
+            <h1>Hello, Dmitry</h1>
+            <h2>У вас 3 записи</h2>
+        </div>
+    );
+}
+
+export default AppHeader;
